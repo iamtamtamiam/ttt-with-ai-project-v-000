ee068ca656e3a1cdd9ca192a9552f0c3647e4ab7
Tam Vu <vutam_a@hotmail.com>
2019-07-21 16:47:37 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,14 +5,33 @@ module Players
     #choices = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     #choices.sample.to_s
     
+      if board.cells[6] == " "
+        "7"
+      elsif board.cells[2] == " "
+        "3"
+      elsif board.cells[0] == " "
+        "1"
+      elsif board.cells[1] == " "
+        "2"
+      elsif board.cells[3] == " "
+        "4"
+      elsif board.cells[7] == " "
+        "8"
+      elsif board.cells[5] == " "
+        "6"
+      elsif board.cells[4] == " "
+        "5"
+      elsif board.cells[8] == " "
+        "9"
+      end
     
-    "6"
-    "2"
-    "0"
-    "1"
-    "3"
-    "7"
-    "5"
+    #"6"
+    #"2"
+    #"0"
+    #"1"
+    #"3"
+    #"7"
+    #"5"
     
     
     
